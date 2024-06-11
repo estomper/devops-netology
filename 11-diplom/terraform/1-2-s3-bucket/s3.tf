f468fe08908ee519201d80c501107aed7b9841f1
alex <alex@ubuntu-22.04.3.myguest.virtualbox.org>
2024-06-11 18:05:31 +0500

    Add diplov v 1.0.1

--- 11-diplom/terraform/1-2-s3-bucket/s3.tf
+++ 11-diplom/terraform/1-2-s3-bucket/s3.tf
@@ -9,34 +9,3 @@ resource "yandex_storage_bucket" "s3-tfstate-netology" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   
-  /*
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = yandex_kms_symmetric_key.key-a.id
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-  */
-}
-/*
-# Create doc Table
-resource "aws_dynamodb_table" "db-tfstate" {
-  name         = "tb-tfstate"
-  billing_mode = "PAY_PER_REQUEST"
-
-  hash_key  = "<attribute_name_1>"
-  range_key = "<attribute_name_2>"
-
-  attribute {
-    name = "<attribute_name_1>"
-    type = "<attribute_type>"
-  }
-
-  attribute {
-    name = "<attribute_name_2>"
-    type = "<attribute_type>"
-  }
-}
-*/
